3e6b38bbb70915f82223acbd5cd918f98a13649b
gameratcraft <143826225+gameratcraft@users.noreply.github.com>
2023-10-06 12:39:00 +0100

    Add files via upload

---
+++ Higher SDD Assesment/Form1.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.BtnEnd = New System.Windows.Forms.Button()
+        Me.LstDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnStart.Location = New System.Drawing.Point(12, 12)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(166, 80)
+        Me.BtnStart.TabIndex = 0
+        Me.BtnStart.Text = "Start"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'BtnEnd
+        '
+        Me.BtnEnd.Font = New System.Drawing.Font("Segoe UI Semibold", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnEnd.Location = New System.Drawing.Point(12, 358)
+        Me.BtnEnd.Name = "BtnEnd"
+        Me.BtnEnd.Size = New System.Drawing.Size(166, 80)
+        Me.BtnEnd.TabIndex = 1
+        Me.BtnEnd.Text = "End"
+        Me.BtnEnd.UseVisualStyleBackColor = True
+        '
+        'LstDisplay
+        '
+        Me.LstDisplay.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LstDisplay.FormattingEnabled = True
+        Me.LstDisplay.ItemHeight = 30
+        Me.LstDisplay.Location = New System.Drawing.Point(184, 12)
+        Me.LstDisplay.Name = "LstDisplay"
+        Me.LstDisplay.Size = New System.Drawing.Size(604, 424)
+        Me.LstDisplay.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LstDisplay)
+        Me.Controls.Add(Me.BtnEnd)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents BtnStart As Button
+    Friend WithEvents BtnEnd As Button
+    Friend WithEvents LstDisplay As ListBox
+End Class
---
+++ Higher SDD Assesment/Form1.vb
@@ -0,0 +1,139 @@
+﻿'Andrew Luca Simpson
+'29/09/2023
+'Higher SDD Assesment Step 7
+'SCN: 211226501
+
+Public Class Form1
+
+    Structure details
+        Dim PupilName As String 'Declares PupilName as a character and stores in a unique location memory
+        Dim CourseWorkMark As Integer 'Declares CourseWorkMark as a whole number and stores in a unique location memory
+        Dim PrelimMark As Integer 'Declares PrelimMark as a whole number and stores in a unique location memory
+    End Structure
+
+    Dim classmarks(15) As details
+
+    Dim PupilName(15) As String 'Declares PupilName as a character and stores in a unique location memory
+    Dim CourseWorkMark(15) As Integer 'Declares CourseWorkMark as a whole number and stores in a unique location memory
+    Dim PrelimMark(15) As Integer 'Declares PrelimMark as a whole number and stores in a unique location memory
+    Dim TotalMark(15) As Integer 'Declares TotalMark as a whole number and stores in a unique location memory
+    Dim Percentage(15) As Integer 'Declares Precentage as a whole number and stores in a unique location memory
+    Dim Grade(15) As String 'Declares Grade as a character and stores in a unique location memory
+    Dim AOcurence As Integer 'Declares Precentage as a whole number and stores in a unique location memory
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+
+        GetDetails(classmarks, PupilName, CourseWorkMark, PrelimMark) 'This subroutine get the details
+        CalculatePercentage(TotalMark, Percentage, CourseWorkMark, PrelimMark) 'This subroutine calculates the percentage
+        CalculateGrade(Percentage, Grade) 'This subroutine calculates the grade
+        countOcurrence() 'This subroutine finds how many A grades there are
+        FindMax(Percentage, classmarks, PupilName)
+        DisplayResults(PupilName, Percentage, Grade) 'This subroutine displays the resulats to the user in a listbox
+
+    End Sub 'Ends the subroutine
+
+    Sub GetDetails(ByRef classmarks() As details, ByRef PupilName() As String, ByRef CourseWorkMark() As Integer, ByRef PrelimMark() As Integer) 'This subroutine get the details from the user
+
+        Dim filename As String
+
+        filename = "H:\Visual Studio\SDD Assessment\classmarks.csv"
+
+        FileOpen(1, filename, OpenMode.Input)
+
+        For i = 0 To 14
+
+            Input(1, classmarks(i).PupilName)
+            Input(1, classmarks(i).CourseWorkMark)
+            Input(1, classmarks(i).PrelimMark)
+
+        Next
+
+        FileClose(1)
+
+    End Sub 'Ends the subroutine
+
+    Sub CalculatePercentage(ByRef TotalMark() As Integer, ByRef Percentage() As Integer, ByVal CourseWorkMark() As Integer, ByVal PrelimMark() As Integer) 'This subroutine calculates the percentage
+
+        For i = 0 To 14
+
+            TotalMark(i) = classmarks(i).CourseWorkMark + classmarks(i).PrelimMark 'This adds the CourseWorkMark and the PrelimMark together to get TotalMark
+
+            Percentage(i) = TotalMark(i) * 100 \ 150 'This takes the TotalMark and times it by 100 then divides it by 150 to get the percentage
+
+        Next
+
+    End Sub 'Ends the subroutine
+
+    Sub CalculateGrade(ByVal Percentage() As Integer, ByRef Grade() As String) 'This subroutine calculates the grade
+
+        For i = 0 To 14
+
+            If Percentage(i) >= 70 Then 'Starts an if statment
+                Grade(i) = "A"
+            ElseIf Percentage(i) >= 60 And Percentage(i) <= 69 Then
+                Grade(i) = "B"
+            ElseIf Percentage(i) >= 50 And Percentage(i) <= 59 Then
+                Grade(i) = "C"
+            ElseIf Percentage(i) >= 45 And Percentage(i) <= 49 Then
+                Grade(i) = "D"
+            Else
+                Grade(i) = "No Grade"
+
+            End If 'Ends the if statment
+
+        Next
+
+    End Sub 'Ends the subroutine
+
+    Sub countOcurrence()
+
+        For i = 0 To 14
+
+            If Percentage(i) >= 70 Then 'Starts an if statment
+                AOcurence = AOcurence + 1 'Adds 1 to AOcurence
+            End If 'Ends the if statment
+
+        Next
+
+        LstDisplay.Items.Add("There are " & AOcurence & " A Grades") 'Sends text and AOcurence to display
+        LstDisplay.Items.Add(" ")
+
+    End Sub
+
+    Sub FindMax(ByVal Percentage() As Integer, ByVal classmarks() As details, ByVal PupilName() As String)
+
+        Dim max As Integer 'Declares max as a whole number and stores in a unique location memory
+        Dim position As Integer 'Declares position as a whole number and stores in a unique location memory
+
+        max = Percentage(0)
+
+        For i = 1 To 14
+
+            If Percentage(i) > max Then
+                max = Percentage(i)
+                position = i
+            End If
+
+        Next
+
+        LstDisplay.Items.Add("The pupil with the top mark is " & classmarks(position).PupilName & " with " & Percentage(position) & "%") 'Sends text, PupilName and percentage to display
+        LstDisplay.Items.Add(" ")
+
+    End Sub
+    Sub DisplayResults(ByVal PupilName() As String, ByVal Percentage() As Integer, ByVal Grade() As String) 'This subroutine displays the resulats to the user in a listbox
+
+        For i = 0 To 14
+
+            LstDisplay.Items.Add(classmarks(i).PupilName) 'Sends PupilName to display
+            LstDisplay.Items.Add("Percentage is " & Percentage(i) & "%") 'Sends text and percentage to display
+            LstDisplay.Items.Add("Grade is " & Grade(i)) 'Sends text and Grade to display
+            LstDisplay.Items.Add(" ")
+
+        Next
+
+    End Sub 'Ends the subroutine
+
+    Private Sub BtnEnd_Click(sender As Object, e As EventArgs) Handles BtnEnd.Click
+        Me.Close()
+    End Sub 'Ends the subroutine
+End Class
